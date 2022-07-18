14ccc53ca2b68f3af9fdb73657a36d9bf956ad24
GustaKowai <gusta.kowai@gmail.com>
2022-07-18 16:11:59 -0300

    Adiciona as estrelas no rating.

--- course-manager/src/app/courses/star/star.component.ts
+++ course-manager/src/app/courses/star/star.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
-  selector: 'app-star'
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls:  ['./star.component.css']
 })
 
 export class StarComponent implements OnChanges{
 
   @Input()
-  rating: number = 1;
+  rating: number = 0;
 
   starWidth: number;
 
   
   ngOnChanges(): void {
-    this.starWidth = this.rating*94/5;
+    this.starWidth = this.rating*74/5;
   }
 
 }
